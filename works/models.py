a3982d48199523929e3467b767577df1969215ef
Evgeniy <trofimovevgeniy9@gmail.com>
2022-03-19 16:52:38 +0300

    Initial commit

---
+++ works/models.py
@@ -0,0 +1,63 @@
+import datetime
+
+from django.db import models
+
+from works.consts import STATUSES, NOT_STARTED_CODE
+
+
+class Subject(models.Model):
+    """Предмет"""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200,
+        null=False, blank=False,
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = 'Предмет',
+        verbose_name_plural = 'Предметы'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+
+class Work(models.Model):
+    """Работа"""
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200,
+        null=False, blank=False,
+    )
+
+    subject = models.ForeignKey(
+        to=Subject,
+        verbose_name='Предмет',
+        related_name='works',
+        on_delete=models.CASCADE,
+        null=False, blank=False,
+    )
+
+    deadline = models.DateField(
+        verbose_name='Дата выполнения',
+        null=True, blank=True,
+    )
+
+    status = models.IntegerField(
+        verbose_name='Статус',
+        null=False, blank=False,
+        choices=STATUSES,
+        default=NOT_STARTED_CODE,
+    )
+
+    class Meta:
+        verbose_name = 'Работа'
+        verbose_name_plural = 'Работы'
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def expired(self):
+        return datetime.datetime.now() > self.deadline
