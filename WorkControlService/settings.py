ba80f76841bc1a7cd9c2977c13f995e6dfcb5dc9
Dmitry <dimaassdf5@gmail.com>
2022-04-20 22:51:51 +0300

    Исправлено Logo страницы и сместил кнопку от таблицы

--- WorkControlService/settings.py
+++ WorkControlService/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'work_control_service_new',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '!Ghfqv2002',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
